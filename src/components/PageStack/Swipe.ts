fd481b8078080166be8a661be0cdb604dff14eba
wholesomedev <msafi+hollowverse@msafi.com>
2017-12-29 22:12:11 -0800

    Give up on unreliable touch event reporting by Safari while a page is scrolling

--- src/components/PageStack/Swipe.ts
+++ src/components/PageStack/Swipe.ts
@@ -19,34 +19,48 @@ export class Swipe {
     we start looking at the `scroll` event on our element for MONITOR_PERIOD of time.
     Based on where it is moving, we decide whether it was a swipe right or a swipe left.
     */
-    const touchStarts$ = Rx.Observable.fromEvent(
-      this.element,
-      'touchstart',
-    ).map(mapTouchEvent as any);
-    const touchMoves$ = Rx.Observable.fromEvent(this.element, 'touchmove').map(
-      mapTouchEvent as any,
-    );
-    const touchEnds$ = Rx.Observable.fromEvent(this.element, 'touchend').map(
-      mapTouchEvent as any,
-    );
 
-    const completeTouches$ = touchStarts$
-      .map(touchStart =>
-        touchMoves$
-          .do(() => {
-            console.log('move');
-          })
-          .takeUntil(touchEnds$)
-          .buffer(Rx.Observable.merge(Rx.Observable.interval(400), touchEnds$))
-          .last(),
-      )
+    this.element.addEventListener('touchstart', e => {
+      e.stopImmediatePropagation();
+      e.stopPropagation();
+      console.log('touchstart');
+    });
+
+    const touchEnd$ = Rx.Observable.fromEvent(this.element, 'touchend').do(
+      () => {
+        // console.log('touchend');
+      },
+    );
+    const scroll$ = Rx.Observable.fromEvent(this.element, 'scroll').do(() => {
+      console.log('scroll');
+    });
+
+    const swipe$ = touchEnd$
+      .map(() => scroll$.takeUntil(Rx.Observable.interval(10).first()))
+      // .switch()
+      .map(x => x.count())
       .switch();
 
-    completeTouches$.subscribe(e => {
-      e.forEach(es => {
-        console.log('e', es.x, es.timestamp);
-      });
+    swipe$.subscribe(e => {
+      console.log('e', e);
     });
+    // const completeTouches$ = touchStarts$
+    //   .map(touchStart =>
+    //     touchMoves$
+    //       .do(() => {
+    //         console.log('move');
+    //       })
+    //       .takeUntil(touchEnds$)
+    //       .buffer(Rx.Observable.merge(Rx.Observable.interval(400), touchEnds$))
+    //       .last(),
+    //   )
+    //   .switch();
+
+    // completeTouches$.subscribe(e => {
+    //   e.forEach(es => {
+    //     console.log('e', es.x, es.timestamp);
+    //   });
+    // });
 
     // completeTouches$.subscribe(e => {
     //   console.log('e', e);
